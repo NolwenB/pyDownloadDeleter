82c5c2e48c54e27b54ef93a04c06ee20aae7c5de
neb0003 <nbachtle@outlook.com>
2020-06-24 23:05:20 -0400

    Deletes files
    Acutally deletes files now. Asks if you want to delete them beforehand.

--- DownloadDeleter.py
+++ DownloadDeleter.py
@@ -1,5 +1,14 @@
 import getpass, os, time
+
 # init variables
+def delete_input():
+    canDelete = input("Delete these files? (y/n): ").upper()
+    if canDelete == "Y":
+        return True
+    elif canDelete == "N":
+        return False
+    print("Try again dawg.")
+    return delete_input()
 delete_time = 172800 # TWO DAYS IN SECONDS
 
 # Creates / Reads the paths.txt file in the same directory as the py file
@@ -12,17 +21,29 @@ except:
     pathsTXT = open("paths.txt", "r")
 
 paths = pathsTXT.read().split("\n")
-
-onlyfiles = [f for f in os.listdir(paths[0]) if os.path.isfile(os.path.join(paths[0], f))]
-
-#print(*onlyfiles, sep="\n-- ")
-
-for b in paths:
+# Formats the path correctly if there is not a '\' at the end of the path
+for x in range(len(paths)):
     try:
-        if b[-1] != "/" or b[-1] != "\\":
-            b += "\\"
+        if paths[x][-1] != "/" or paths[x][-1] != "\\":
+            paths[x] += "\\"
     except:
         pass
+
+# puts all file names in the directories into a list
+onlyfiles = [f for f in os.listdir(paths[0]) if os.path.isfile(os.path.join(paths[0], f))]
+file_amount = 0
+for b in paths:
+    print("\t")
     for a in onlyfiles:
-        print(f"** {a} \t\t: {time.time()-os.path.getmtime(b+a)}")
-print(f"** {len(onlyfiles)} items")
+        if (time.time()-os.path.getmtime(b+a) >= delete_time):
+            file_amount += 1
+            print(f"** {a} \t\t: {time.time()-os.path.getmtime(b+a)}")
+# Asks users if the files can be deleted
+if file_amount == 0 or not delete_input():
+    quit()
+
+for b in paths:
+    print("\t")
+    for a in onlyfiles:
+        if (time.time()-os.path.getmtime(b+a) >= delete_time):
+            os.remove(b+a)
